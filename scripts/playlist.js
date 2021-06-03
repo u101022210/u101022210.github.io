d025642f3171f78b601c4a2a5fc2d8475eff435b
u101022210 <mike5227201@gmail.com>
2021-06-03 23:35:59 +0800

    song update

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -4,4 +4,59 @@
 		artist:"菅野洋子",
 		src:"music/Tank!.mp3"
 	},
+	{
+		name:"3AM Radio",
+		artist:"2 Mello",
+		src:"music/3AM Radio.mp3"
+	},
+	{
+		name:"18 BB's Theme",
+		artist:"2 Mello",
+		src:"music/18 BB's Theme.mp3"
+	},
+	{
+		name:"Afternoon Storm",
+		artist:"2 Mello",
+		src:"music/Afternoon Storm.mp3"
+	},
+	{
+		name:"Come Closer",
+		artist:"2 Mello",
+		src:"music/Come Closer.mp3"
+	},
+	{
+		name:"Garden Vibes",
+		artist:"2 Mello",
+		src:"music/Garden Vibes.mp3"
+	},
+	{
+		name:"Rainy Season",
+		artist:"2 Mello",
+		src:"music/Rainy Season.mp3"
+	},
+	{
+		name:"See U Next Time",
+		artist:"2 Mello",
+		src:"music/See U Next Time.mp3"
+	},
+	{
+		name:"Shared Memory",
+		artist:"2 Mello",
+		src:"music/Shared Memory.mp3"
+	},
+	{
+		name:"Summer Walk",
+		artist:"2 Mello",
+		src:"music/Summer Walk.mp3"
+	},
+	{
+		name:"Swimming Lights",
+		artist:"2 Mello",
+		src:"music/Swimming Lights.mp3"
+	},
+	{
+		name:"The Full Mix",
+		artist:"2 Mello",
+		src:"music/The Full Mix.mp3"
+	},
 ];
